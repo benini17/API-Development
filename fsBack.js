a77b6927574de285cf527f0ef4494b891883dd14
Benini17 <abeninit@gmail.com>
2020-07-28 19:05:05 -0300

    convert each state into JSON file + its own cities

--- fsBack.js
+++ fsBack.js
@@ -6,28 +6,38 @@ async function arrCitiesNStates() {
   try {
     const statesData = JSON.parse(await fs.readFile('Estados.json'));
     const citiesData = JSON.parse(await fs.readFile('Cidades.json'));
-    console.log(statesData);
+    // console.log(statesData);
     // console.log(citiesData);
 
     let arrCities = [];
 
     for (let i = 0; i < statesData.length; i++) {
-      await fs.writeFile(`${createStateJSON(i)}.json`);
+      for (let index = 0; index < citiesData.length; index++) {
+        let city = citiesData[index];
+        if (statesData[i].ID == city.Estado) {
+          arrCities.push(city.Nome);
+        }
+      }
+      console.log(arrCities);
+      await JSON.stringify(
+        fs.writeFile(`${createStateJSON(i)}.json`, JSON.stringify(arrCities))
+      );
+      arrCities = [];
     }
 
     function createStateJSON(i) {
       return statesData[i].Sigla;
     }
 
-    function stateReceiver() {
-      citiesData.map((Nome) => Nome.Estado);
-      statesData.map((Nome) => Nome.ID);
+    // function stateReceiver() {
+    //   citiesData.map((Nome) => Nome.Estado);
+    //   statesData.map((Nome) => Nome.ID);
 
-      if (Nome == Nome.Estado) {
-        arrCities.push(citiesData[y].Nome);
-      }
-    }
-    console.log(arrCities);
+    //   if (Nome == Nome.Estado) {
+    //     arrCities.push(citiesData[y].Nome);
+    //   }
+    // }
+    // console.log(arrCities);
   } catch (error) {
     console.log(error);
   }
